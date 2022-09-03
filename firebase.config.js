fe8ba6c78e9029a1c7c56c8cfcb522f4ebc681b3
Ankit Srivastava <ankitsrivas14@gmail.com>
2022-09-04 01:29:52 +0530

    code cleanup and firebase init

---
+++ firebase.config.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBm1GJ58dhue7BnfDq0y8oZbOWAxuCNwnA",
+  authDomain: "house-marketplace-react-8b093.firebaseapp.com",
+  projectId: "house-marketplace-react-8b093",
+  storageBucket: "house-marketplace-react-8b093.appspot.com",
+  messagingSenderId: "584468760730",
+  appId: "1:584468760730:web:b631b8b4a8b542b8853723"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
