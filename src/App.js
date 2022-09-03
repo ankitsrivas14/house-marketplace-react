d4649d9be49bd96ae6874d52ef490b5136d9f19a
Ankit Srivastava <ankitsrivas14@gmail.com>
2022-09-04 02:17:00 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 
+//COMPONENTS
+import Navbar from './components/Navbar';
+
 
 function App() {
     return (
@@ -21,6 +24,7 @@ function App() {
                 <Route path="/sign-up" element={<SignUp />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
             </Routes>
+            <Navbar />
         </Router>
     );
 }
