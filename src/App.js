a88c3c102fd3e34ce79620ecaaa9a4e59f7540d8
Ankit Srivastava <ankitsrivas14@gmail.com>
2022-09-04 02:05:21 +0530

    created pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+//REACT
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+//PAGES
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
 
 
 function App() {
-  return (
-    <>
-      <h1>My App</h1>
-    </>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Explore />} />
+                <Route path="/offers" element={<Offers />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
