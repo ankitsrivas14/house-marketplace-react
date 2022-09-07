ca84708bc8dfaa65768c0823234914f3cb56c75d
Ankit Srivastava <ankitsrivas14@gmail.com>
2022-09-07 17:07:59 +0530

    created listing screen and added contact form

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 //COMPONENTS
 import Navbar from './components/Navbar';
@@ -33,6 +35,8 @@ function App() {
                 <Route path="/sign-up" element={<SignUp />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/create-listing" element={<CreateListing />} />
+                <Route path="/category/:categoryName/:listingId" element={<Listing />} />
+                <Route path="/contact/:landlordId" element={<Contact />} />
             </Routes>
             <Navbar />
             <ToastContainer />
